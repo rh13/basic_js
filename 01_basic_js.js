1a4c892091df8aa847f241fdcaa4d9772a087757
rh13 <rhlikhon13@gmail.com>
2018-05-04 03:21:54 +0600

    Javascrip for 01_basic_js_syntex

---
+++ 01_basic_js.js
@@ -0,0 +1,22 @@
+document.write("The Javascript");
+alert("Hello World!")
+
+
+function myFunction() {
+	var abc=prompt("write your name:");
+	if (abc != null) {
+        document.getElementById("demo").innerHTML =
+        "Hello " + abc + "! How are you today?";
+       }
+}
+
+function myF() {
+
+	var x, y, z;  
+    x = 5;
+    y = 6; 
+    z = x + y; 
+   document.getElementById("demon").innerHTML = "The value is: "+ z ; 
+}
+
+ 
